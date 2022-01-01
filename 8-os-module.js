a8773ae76d61f455f1631de39cd32996caab8f23
ddrichmond1998 <drichmondhealthexec@gmail.com>
2021-12-31 22:15:36 -0600

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,16 @@
+const os = require('os');
+
+//info about current user
+const user= os.userInfo()
+console.log(user);
+
+//return system uptime in seconds
+console.group(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+}
+console.log(currentOS);
---
+++ app.js
@@ -0,0 +1,7 @@
+//npm init -y to install package.json
+
+const _ = require('lodash');
+
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
